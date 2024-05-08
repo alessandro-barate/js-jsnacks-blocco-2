f99571f49419d714f75503df16a208da959be33a
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-08 17:17:30 +0200

    [UPD] Snack B main logic almost done

--- snack-b/javascript/main.js
+++ snack-b/javascript/main.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const array = ["pane", "pasta", "acqua", "sale", "pomodoro", "tonno", "pepe"];
+
+const inputElement = "pane";
+
+for (let counter = 0; counter < array.length; counter++) {
+  if (array[counter] === inputElement) {
+    console.log(counter);
+  }
+}
