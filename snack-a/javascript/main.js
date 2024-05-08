238224958923df28a10d92a8c17b21268bebad67
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-08 15:53:28 +0200

    [UPD] Snack a working on the logic

--- snack-a/javascript/main.js
+++ snack-a/javascript/main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const elementi = [1, 2, 3, 4, 5, 6, 7, 8];
+
+let counter = 0;
+let string;
+
+while (elementi[counter] !== undefined) {
+  string += elementi[counter];
+  console.log(elementi[counter]);
+  counter++;
+  //console.log(string);
+}
