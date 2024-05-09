84c0cd58262cb71f0566faa12bb89e3a92f67927
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-09 11:37:45 +0200

    [UPD] Snack Bonus done

--- snack-bonus/javascript/main.js
+++ snack-bonus/javascript/main.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const arrayA = [1, 2, 3, 4, 5, 6, 7];
+let inputElement = "Ciccio";
+
+function inserisciInTesta(array, input) {
+  const arrayB = [input];
+  let arrayLength = array.length;
+
+  for (let i = 0; i < arrayLength; i++) {
+    arrayB[i + 1] = array[i];
+  }
+
+  return arrayB;
+}
+
+inserisciInTesta(arrayA, inputElement);
+
+console.log(inserisciInTesta(arrayA, inputElement));
